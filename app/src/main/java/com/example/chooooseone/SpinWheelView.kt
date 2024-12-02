7dc0212d209d22ed5b47c5ec91d85bfea7dd68fd
Yaser <yaseryjjou@gmail.com>
2024-12-02 10:53:37 +0100

    by default hiden but when i add the first input he didn't display

--- app/src/main/java/com/example/chooooseone/SpinWheelView.kt
+++ app/src/main/java/com/example/chooooseone/SpinWheelView.kt
@@ -13,7 +13,6 @@ import kotlin.math.sin
 
 private const val COMPLETE_ANGLE = 360
 
-
 class SpinWheelView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
@@ -38,21 +37,18 @@ class SpinWheelView @JvmOverloads constructor(
         paint?.style = Paint.Style.FILL
 
         setBackgroundColor(Color.TRANSPARENT)
-
-        addDefaultSector() // Ensure default sector is added with label.
     }
 
-    private fun addDefaultSector() {
+    fun addFirstSector(label: String?) {
         val randomColor = Color.rgb(
             (0..255).random(),
             (0..255).random(),
             (0..255).random()
         )
-        val defaultLabel = "Default Sector"  // Assign a label to the default sector
-        val defaultSector = SectorModel(randomColor, defaultLabel)  // Add label here
-        sectors.add(defaultSector)
+        val firstSector = SectorModel(randomColor, label)
+        sectors.add(firstSector)
         angleEnclosedBySector = COMPLETE_ANGLE / sectors.size
-        requestLayout() // Recalculate layout after adding the default sector
+        requestLayout()
     }
 
     private fun init(
@@ -81,7 +77,7 @@ class SpinWheelView @JvmOverloads constructor(
         val newSector = SectorModel(randomColor, label)
         sectors.add(newSector)
         angleEnclosedBySector = COMPLETE_ANGLE / sectors.size
-        requestLayout() // Recalculate layout after adding a new sector
+        requestLayout()
     }
 
     private var currentRotation = 0f
@@ -122,7 +118,6 @@ class SpinWheelView @JvmOverloads constructor(
             paint?.color = sector.color
             canvas.drawArc(0f, 0f, width.toFloat(), height.toFloat(), sectorAngle, angleEnclosedBySector.toFloat(), true, paint!!)
 
-            // Draw the label for each sector, including the default one
             sector.label?.let {
                 canvas.drawText(it, x, y, textPaint)
             }
@@ -153,7 +148,7 @@ class SpinWheelView @JvmOverloads constructor(
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         val width = MeasureSpec.getSize(widthMeasureSpec)
         val height = MeasureSpec.getSize(heightMeasureSpec)
-        setMeasuredDimension(width, width) // Make the width equal to height to ensure it's circular
+        setMeasuredDimension(width, width)
         radius = width / 2.0f
 
         if (!isInit && sectors.isNotEmpty()) {
